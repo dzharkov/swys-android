069203c571be4593bbab6a934e398bab98c67c18
Denis Zharkov <bintree@eblans.org>
2013-10-14 15:59:22 +0400

    Result Activity's sketch

---
+++ src/ru/spbau/mit/swys/ResultActivity.java
@@ -0,0 +1,26 @@
+package ru.spbau.mit.swys;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import ru.spbau.mit.swys.search.SearchResult;
+import ru.spbau.mit.swys.search.SearchResultItemAdapter;
+
+public class ResultActivity extends Activity {
+    private SearchResult result;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.result);
+
+        result = getIntent().getParcelableExtra("result");
+
+        ListAdapter adapter = new SearchResultItemAdapter(this, R.layout.result_item, result.getItems());
+
+        ListView listView = (ListView)findViewById(R.id.result_list);
+        listView.setAdapter(adapter);
+    }
+}
--- src/ru/spbau/mit/swys/SearchActivity.java
+++ src/ru/spbau/mit/swys/SearchActivity.java
@@ -1,20 +1,48 @@
 package ru.spbau.mit.swys;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import ru.spbau.mit.swys.search.Image;
+import ru.spbau.mit.swys.search.SearchResult;
+import ru.spbau.mit.swys.search.SearchService;
+import ru.spbau.mit.swys.search.StubSearchService;
 
 public class SearchActivity extends Activity {
+
+    private Bitmap currentImageBitmap;
+    private SearchService searchService = new StubSearchService();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
 
         Bitmap bm = getIntent().getExtras().getParcelable("image");
+        currentImageBitmap = bm;
 
         ImageView iv = (ImageView)findViewById(R.id.target_image);
         iv.setImageBitmap(bm);
+    }
+
+    private void searchCurrentImage()
+    {
+        SearchResult result = searchService.search(new Image(currentImageBitmap));
+
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+        resultIntent.putExtra("result", result);
+
+        startActivity(resultIntent);
+    }
+
+    public void searchButtonProcess(View view) {
+        ProgressBar pb = (ProgressBar)findViewById(R.id.progress_bar);
+        pb.setVisibility(View.VISIBLE);
+
+        searchCurrentImage();
     }
 }
