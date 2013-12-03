56b2bbced598c852e71d7b03f53283358cb09e0c
Denis Zharkov <bintree@eblans.org>
2013-12-03 11:44:32 +0400

    CroppingAsyncTask

--- src/ru/spbau/mit/swys/CropImageActivity.java
+++ src/ru/spbau/mit/swys/CropImageActivity.java
@@ -1,8 +1,13 @@
 package ru.spbau.mit.swys;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import ru.spbau.mit.swys.crop.CropImageView;
@@ -17,6 +22,9 @@ public class CropImageActivity extends BaseActivity {
 
     private CropManager cropManager = new CropManager();
 
+    private CroppingAsyncTask currentTask;
+    private ProgressDialog progressDialog;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crop_image);
@@ -29,8 +37,6 @@ public class CropImageActivity extends BaseActivity {
 
         cropImageView.refreshDrawableState();
     }
-
-
 
     private File writeBitmapToTempFile(Bitmap bitmap) {
         //TODO: delete it somehow later
@@ -46,15 +52,51 @@ public class CropImageActivity extends BaseActivity {
     }
 
     public void startCropButtonProcess(View view) {
+        progressDialog = ProgressDialog.show(this,
+            getString(R.string.progress_dialog_title),
+            getString(R.string.progress_dialog_msg),
+            true, true,
+            new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialogInterface) {
+                    currentTask.cancel(true);
+                }
+            }
+        );
+
         CropImageView cropImageView = (CropImageView) findViewById(R.id.image);
 
-        Bitmap croppedBitmap = cropManager.cropBitmap(currentBitmap, cropImageView.getCropPoints());
+        currentTask = new CroppingAsyncTask();
+        currentTask.execute(cropImageView.getCropPoints());
+    }
 
-        File file = writeBitmapToTempFile(croppedBitmap);
+    private void processCroppingCompletion(File imgFile) {
+        if (progressDialog != null) {
+            progressDialog.hide();
+        }
 
         Intent searchIntent = new Intent(this, SearchActivity.class);
-        searchIntent.putExtra(RequestCodes.PICTURE_CROP_EXTRA_FIELD, Uri.fromFile(file));
+        searchIntent.putExtra(RequestCodes.PICTURE_CROP_EXTRA_FIELD, Uri.fromFile(imgFile));
 
         startActivity(searchIntent);
+    }
+
+    private class CroppingAsyncTask extends AsyncTask<Point[],Void,File> {
+        @Override
+        protected File doInBackground(Point[]... points) {
+            Bitmap croppedBitmap = cropManager.cropBitmap(currentBitmap, points[0]);
+
+            return writeBitmapToTempFile(croppedBitmap);
+        }
+
+        @Override
+        protected void onPostExecute(File file) {
+            processCroppingCompletion(file);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
     }
 }
